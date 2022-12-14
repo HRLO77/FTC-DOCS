0ae38c967a6041cb22969d4396767318def3df14
HRLO77 <95102676+HRLO77@users.noreply.github.com>
2022-12-14 15:53:36 -0500

    Update controller.java

--- controller.java
+++ controller.java
@@ -1,54 +1,39 @@
+// this file is for a gamepad controlling the robot with port config as specified in the pad_config.yaml
 package org.firstinspires.ftc.teamcode;
-
+// see https://github.com/HRLO77/FTC-DOCS for documentation written by the 2022/2023 Saltfleet robotics club programming team! (Just me :\)
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import org.firstinspires.ftc.robotcore.external.android.AndroidTextToSpeech;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "controller (Blocks to Java)")
-public class controller extends LinearOpMode {
+@TeleOp(name = "main (main.java compiled)")
+public class main extends LinearOpMode { // quadratic op mode when?
 
-  private AndroidTextToSpeech androidTextToSpeech;
-  private DcMotor Motor1;
+  private Servo claw;
   private DcMotor Motor2;
-
-  /**
-   * This function is executed when this Op Mode is selected from the Driver Station.
-   */
+  private DcMotor Motor1;
+  private DcMotor arm;
+  
   @Override
   public void runOpMode() {
-    androidTextToSpeech = new AndroidTextToSpeech();
-    Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
+    claw = hardwareMap.get(Servo.class, "claw");  // do not move over to private variable declarations, NullPointerException will occur.
     Motor2 = hardwareMap.get(DcMotor.class, "Motor2");
+    Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
+    arm = hardwareMap.get(DcMotor.class, "arm"); 
 
     // Put initialization blocks here.
-    waitForStart();
-    androidTextToSpeech.initialize();
-    if (opModeIsActive()) {
+    waitForStart();  // wait for the robot to start
+    if (opModeIsActive()) { // run when instructed to
       // Put run blocks here.
-      ((DcMotorEx) Motor1).setVelocity(10);
-      ((DcMotorEx) Motor2).setVelocity(10);
+      claw.scaleRange(0.15, 0.7); // Prevent scaling the servo too far (likely overheating, check regularly.)
+      Motor2.setDirection(DcMotorSimple.Direction.REVERSE); // account for weird black-magic fuckery that happens with swapping motor ports.
       while (opModeIsActive()) {
-        if (gamepad1.left_stick_y > 0) {
-          Motor1.setDirection(DcMotorSimple.Direction.REVERSE);
-        } else if (gamepad1.left_stick_y < 0) {
-          Motor1.setDirection(DcMotorSimple.Direction.FORWARD);
-        } else {
-        }
-        if (gamepad1.right_stick_y > 0) {
-          Motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-        } else if (gamepad1.right_stick_y < 0) {
-          Motor2.setDirection(DcMotorSimple.Direction.FORWARD);
-        } else {
-        }
-        Motor1.setPower(gamepad1.left_stick_y);
-        Motor2.setPower(gamepad1.right_stick_y);
-        telemetry.update();
+        claw.setPosition(gamepad1.left_stick_x); // The claw is left joystick, left and right
+        arm.setPower(gamepad1.right_stick_x); // The arm is the right joy stick, left and right
+        Motor2.setPower(gamepad1.right_stick_y); // the right tire is the right joystick, up and down
+        Motor1.setPower(gamepad1.left_stick_y); // the left tire is the left joystick, up and down
       }
     }
-
-    androidTextToSpeech.close();
   }
 }
